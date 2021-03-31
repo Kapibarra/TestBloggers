405d2b69a64375f74feaa37fd83251d03598f7c2
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-03-31 12:29:35 +0300

    init project

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import "./scss/index.scss";
+
+/* burger */
+(function () {
+  const hamburger = {
+    navToggle: document.querySelector('.nav-toggle'),
+    nav: document.querySelector('nav'),
+
+    doToggle: function (e) {
+      e.preventDefault();
+      this.navToggle.classList.toggle('expanded');
+      this.nav.classList.toggle('expanded');
+    },
+  };
+  hamburger.navToggle.addEventListener('click', function (e) { hamburger.doToggle(e); });
+  // hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
+}());
+
+function burgerButtonToggle() {
+  const burgerButton = document.querySelector(".nav-toggle")
+  const yPos = window.scrollY;
+  if (yPos > 10) {
+    burgerButton.classList.add("unvisible")
+    burgerButton.classList.remove("visible");
+  } else {
+    burgerButton.classList.remove("unvisible");
+    burgerButton.classList.add("visible");
+  }
+}
+window.addEventListener("scroll", burgerButtonToggle);
